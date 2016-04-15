5a4bb538bb515805283da552af9d15093b122d1d
sejong park <devsejong@gmail.com>
2016-04-16 08:45:53 +0900

    .

---
+++ src/main/java/kr/chandol/java8inaction/chapter13/selfstudy/SampleList.java
@@ -0,0 +1,27 @@
+package kr.chandol.java8inaction.chapter13.selfstudy;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by devse on 2016-04-15.
+ */
+public class SampleList {
+
+    public void aa(){
+List<Integer> numbers = Arrays.asList(1,2,3,4,5);
+List<Integer> anotherNumbers = numbers;
+
+anotherNumbers.add(10);
+
+assertThat(anotherNumbers.size(), is(6));
+assertThat(numbers.size(), is(5));
+    }
+}
